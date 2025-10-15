920945cad057b0689b3d128090f87b0a56f2e088
mukabwag <mukabwaglo@gmail.com>
2025-10-15 16:22:05 +0300

    Home Page. Still incomplete though

---
+++ php/index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="icon" href="" type="image/x-ico">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="../images/black image.jpeg" alt="">
+            <p>MUKABWA'S FARM</p>
+        </div>
+        <div class="pages">
+            <ul>
+                <li><a>HOME</a></li>
+                <li><a>ABOUT US</a></li>
+                <li><a>PRODUCTS</a></li>
+                <li><a>TESTIMONIALS</a></li>
+                <li><a>CONTACTS</a></li>
+                <li><a>LOG IN</a></li>
+                <li><a>SIGN UP</a></li>
+            </ul>
+        </div>
+    </header>
+    <main>
+    <!--Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
+        <section class="home">
+            <div>
+                <h1>MUKABWA'S FARM</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+            </div>
+        </section>
+
+        <section class="about-us">
+            <div class="image">
+                <img src="../images/black image.jpeg" alt="">
+            </div>
+            <div class="text">
+                <h1 class="title">About Us</h1><!--h1 is not an inline elememt so u don't need to put it in a div to specify block element properties-->
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </div>
+                <div class="button">
+                    <button>SEE MORE &RightArrow;</button>
+                </div>
+            </div>
+        </section>
+
+        <section class="vision">
+            <div class="upper">
+                <div class="title">
+                    <h1>Our</h1>
+                    <h1>Vision</h1>
+                </div>
+                <div class="image">
+                    <img src="../images/black image.jpeg" alt="">
+                </div>
+            </div>
+            <div class="lower">
+                <div class="image">
+                    <img src="../images/black image.jpeg" alt="">
+                </div>
+                <div class="text"> 
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="products">
+            <h1 class="title">Our Products</h1>
+            <div class="example">
+                <div class="image">
+                    <img src="../images/black image.jpeg" alt="">
+                </div>
+                <div class="about">
+                    <h1>Dairy</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <button>READ MORE</button>
+                </div>
+            </div>
+        </section>
+
+        <section class="testimonials">
+            <h1>Testimonials</h1>
+            <div class="content">
+                <div>
+                    <img src="../images/black image.jpeg" alt="">
+                    <h2>Name</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div>
+                    <img src="../images/black image.jpeg" alt="">
+                    <h2>Name</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div>
+                    <img src="../images/black image.jpeg" alt="">
+                    <h2>Name</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="contacts">
+            <div>
+                <h1>Get In Touch</h1>
+            </div>
+            <ul>
+                <li>
+                    <div>
+                        <img src="../images/black image.jpeg" alt="">
+                        <h2>Phone Number</h2>
+                        <p>+254729183672</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <img src="../images/black image.jpeg" alt="">
+                        <h2>Email</h2>
+                        <p>mukabwasfarm@gmail.com</p>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <div>
+                        <img src="../images/black image.jpeg" alt="">
+                        <h2>Address</h2>
+                        <p>Kakamega</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <img src="../images/black image.jpeg" alt="">
+                        <h2>Form</h2>
+                        <a href="#">Click Me</a>
+                    </div>
+                </li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; Mukabwa's farm</p>
+    </footer>
+</body>
+</html>
