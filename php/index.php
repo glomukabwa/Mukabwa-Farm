60301830044f7a4dff346eee45a130741ade8794
mukabwag <mukabwaglo@gmail.com>
2025-10-16 17:25:08 +0300

    Almost complete template. We're left with colors and images and the products sliding thing

--- php/index.php
+++ php/index.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="../css/index.css">
 </head>
 <body>
+    <div class="landingpage">
     <header>
         <div class="logo">
             <img src="../images/black image.jpeg" alt="">
@@ -35,10 +36,11 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
             </div>
         </section>
+</div>    
 
         <section class="about-us">
             <div class="image">
-                <img src="../images/black image.jpeg" alt="">
+                <img src="../images/maize_plantation.jpg" alt="">
             </div>
             <div class="text">
                 <h1 class="title">About Us</h1><!--h1 is not an inline elememt so u don't need to put it in a div to specify block element properties-->
@@ -73,15 +75,15 @@
         </section>
 
         <section class="products">
-            <h1 class="title">Our Products</h1>
+            <h1>Our Products</h1>
             <div class="example">
                 <div class="image">
                     <img src="../images/black image.jpeg" alt="">
                 </div>
                 <div class="about">
-                    <h1>Dairy</h1>
+                    <h2>Dairy</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <button>READ MORE</button>
+                    <button>READ MORE &RightArrow;</button>
                 </div>
             </div>
         </section>
@@ -90,59 +92,53 @@
             <h1>Testimonials</h1>
             <div class="content">
                 <div>
-                    <img src="../images/black image.jpeg" alt="">
+                    <img src="../images/profile_icon.png" alt="profile icon">
                     <h2>Name</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
                 </div>
                 <div>
-                    <img src="../images/black image.jpeg" alt="">
+                    <img src="../images/profile_icon.png" alt="profile icon">
                     <h2>Name</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
                 </div>
                 <div>
-                    <img src="../images/black image.jpeg" alt="">
+                    <img src="../images/profile_icon.png" alt="profile icon">
                     <h2>Name</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
                 </div>
             </div>
         </section>
 
         <section class="contacts">
-            <div>
+            <div class="content">
                 <h1>Get In Touch</h1>
+             <div class="info">
+                <div class="left">
+                 <div>
+                    <img src="../images/phone_icon.png" alt="phone icon">
+                    <h2>Phone Number</h2>
+                    <p>+254729183672</p>
+                 </div>
+                 <div>
+                    <img src="../images/home_icon.png" alt="home icon">
+                    <h2>Address</h2>
+                    <p>Kakamega</p>
+                 </div>
+                </div>
+                <div class="right">
+                 <div>
+                    <img src="../images/email_icon.png" alt="email icon">
+                    <h2>Email</h2>
+                    <p>mukabwasfarm@gmail.com</p>
+                 </div>
+                 <div>
+                    <img src="../images/form_icon.png" alt="form icon">
+                    <h2>Form</h2>
+                    <a href="#">Click Me</a>
+                 </div>
+                </div>
+             </div>
             </div>
-            <ul>
-                <li>
-                    <div>
-                        <img src="../images/black image.jpeg" alt="">
-                        <h2>Phone Number</h2>
-                        <p>+254729183672</p>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <img src="../images/black image.jpeg" alt="">
-                        <h2>Email</h2>
-                        <p>mukabwasfarm@gmail.com</p>
-                    </div>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <div>
-                        <img src="../images/black image.jpeg" alt="">
-                        <h2>Address</h2>
-                        <p>Kakamega</p>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <img src="../images/black image.jpeg" alt="">
-                        <h2>Form</h2>
-                        <a href="#">Click Me</a>
-                    </div>
-                </li>
-            </ul>
         </section>
     </main>
     <footer>
