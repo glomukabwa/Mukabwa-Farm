58217ae23595bb27469c6e53ae443e42bf067c4c
mukabwag <mukabwaglo@gmail.com>
2025-10-20 10:45:56 +0300

    Changes

--- php/index.php
+++ php/index.php
@@ -144,6 +144,19 @@
                         </div>
                     </div>
 
+                    <div class="swiper-slide">
+                        <div class="swiper-content">
+                            <div class="image">
+                                <img src="../images/Kales.jpg" alt="Kales">
+                            </div>
+                            <div class="about">
+                                <h2>Kales</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <button>READ MORE</button>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
                 <!-- Navigation buttons -->
